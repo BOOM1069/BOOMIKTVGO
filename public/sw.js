a9346b072459b1fcc7be2def5624e38b531459f1
BOOM1069 <marioravello1@gmail.com>
2026-05-02 16:46:37 -0600

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+// This minimal Service Worker satisfies the PWA install requirement for Android/Chrome.
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Pass through all network requests normally
+});
